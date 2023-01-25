7a9a699694a873eb1656dfc1e2ea42a867a2bca8
Gustavo Machado Mette <gustavo.mette@gmail.com>
2023-01-25 15:30:55 -0300

    .

---
+++ my-app/src/component/navbar.jsx
@@ -0,0 +1,22 @@
+
+import styles from '../styles/navbar.module.css'
+
+
+export default function Navbar() {
+    return (
+        <div>
+<navbar>
+<img className="logo" src="image/Logo.png"></img>
+<div className="menu-btn">
+    <i class="fa fa-bars fa-2x" onclick="menuShow()"></i>
+</div>
+<ul>
+    
+    <li> <a href="#">Parceiros</a></li>
+    <li> <a href="#" class="active">COMECE AQUI</a></li>
+</ul>
+</navbar>
+
+</div>
+)
+}
